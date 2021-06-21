65ad8837aff766a821e45f04c9b691ca33f10bac
charles paul requena palomino <rcharles84@gmail.com>
2021-06-21 11:51:45 -0500

    delete addons factruralo pe

--- l10n_pe_facturalo/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account
-from . import res
-from . import stock
-from . import res_config_settings
-from . import account_invoice
-from . import account_invoice
